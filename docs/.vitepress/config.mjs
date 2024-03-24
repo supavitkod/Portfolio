4a7e2281540a9694ab0c96d1560830d07a7d6a7f
Pornwipa Nilbodee <pornwipa.nil@gmail.com>
2024-03-24 19:37:23 +0700

    use mjs file

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/portfolio/',
   title: "Supavit's Portfolio",
   description: "Explore my data analysis portfolio! Discover how I turn raw data into actionable insights through visualization and predictive modeling. Let's dive into the world of data together!",
   themeConfig: {
@@ -25,5 +24,6 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
-  }
+  },
+  base: '/portfolio/',
 })
